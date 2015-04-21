9534a0a5d3333fcbce7cf69cb6dab5db835d9378
Peter Johnson <peter@peterjohnson.io>
2015-04-20 21:29:38 -0400

    further refactored rtm stuff into its own module

--- main.go
+++ main.go
@@ -1,25 +1,28 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
-	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
-	"time"
 
-	"golang.org/x/net/websocket"
+	"github.com/nejstastnejsistene/frotz-slack-bot/rtm"
 )
 
-type Message map[string]interface{}
+func onMessage(msg rtm.Message, respond chan rtm.Message) {
+	if !directMessage(msg) {
+		return
+	}
+	user := msg["user"].(string)
+	text := msg["text"].(string)
+	log.Printf("%s: \"%s\"\n", user, text)
+	respond <- rtm.Message{
+		"type":    msg["type"],
+		"channel": msg["channel"],
+		"text":    msg["text"],
+	}
+}
 
-func (m Message) Type() string    { return m["type"].(string) }
-func (m Message) Channel() string { return m["channel"].(string) }
-func (m Message) User() string    { return m["user"].(string) }
-func (m Message) Text() string    { return m["text"].(string) }
-
-func (m Message) DirectMessage() bool {
+func directMessage(m rtm.Message) bool {
+	// Ignore reply_to messages; these are for already sent messages.
 	if _, ok := m["reply_to"]; ok {
 		return false
 	}
@@ -40,123 +43,10 @@ func (m Message) DirectMessage() bool {
 	return msgType == "message" && len(channel) > 0 && channel[0] == 'D'
 }
 
-func (m Message) String() string {
-	buf, _ := json.Marshal(m)
-	return string(buf)
-}
-
-var token string
-
-func init() {
+func main() {
+	var token string
 	if token = os.Getenv("TOKEN"); token == "" {
 		log.Fatal("TOKEN not specified")
 	}
-}
-
-func webSocketUrl() (url string, err error) {
-	log.Println("authenticating with rtm")
-	resp, err := http.Get("https://slack.com/api/rtm.start?token=" + token)
-	if err != nil {
-		return
-	}
-	defer resp.Body.Close()
-	buf, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return
-	}
-	var body map[string]interface{}
-	if err = json.Unmarshal(buf, &body); err != nil {
-		return
-	}
-	url, ok := body["url"].(string)
-	if !ok {
-		err = errors.New("unable to parse rtm url")
-	}
-	return
-}
-
-func connectToRtm() (ws *websocket.Conn, input, output chan Message, err error) {
-	var url string
-	url, err = webSocketUrl()
-	if err != nil {
-		return
-	}
-
-	log.Println("connecting to rtm")
-	origin := "http://localhost/"
-	ws, err = websocket.Dial(url, "", origin)
-	if err != nil {
-		return
-	}
-
-	input = make(chan Message)
-	go func() {
-		for {
-			var msg Message
-			if err := websocket.JSON.Receive(ws, &msg); err != nil {
-				close(input)
-				return
-			}
-			if msg != nil {
-				input <- msg
-			}
-		}
-	}()
-
-	output = make(chan Message)
-	go func() {
-		id := 0
-		for {
-			msg := <-output
-			msg["id"] = id
-			if err := websocket.JSON.Send(ws, msg); err != nil {
-				close(output)
-				return
-			}
-			id++
-		}
-	}()
-
-	return
-}
-
-func main() {
-	ws, input, output, err := connectToRtm()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer ws.Close()
-
-	messages := make(chan Message)
-	go func() {
-		for {
-			select {
-			case msg, ok := <-input:
-				// Handle disconects.
-				if !ok {
-					log.Println("disconnected from rtm")
-					ws, input, output, err = connectToRtm()
-					if err != nil {
-						log.Fatal(err)
-					}
-					defer ws.Close()
-					continue
-				}
-				if msg.DirectMessage() {
-					messages <- msg
-				}
-			case <-time.After(5 * time.Second):
-				output <- Message{"type": "ping"}
-			}
-		}
-	}()
-
-	for m := range messages {
-		log.Println(m)
-		output <- Message{
-			"type":    m.Type(),
-			"channel": m.Channel(),
-			"text":    m.Text(),
-		}
-	}
+	rtm.LoopForever(token, onMessage)
 }
---
+++ rtm/rtm.go
@@ -0,0 +1,128 @@
+package rtm
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	"golang.org/x/net/websocket"
+)
+
+var (
+	origin  = "http://localhost/"
+	timeout = 5 * time.Second
+)
+
+type Message map[string]interface{}
+
+func (m Message) String() string {
+	buf, _ := json.Marshal(m)
+	return string(buf)
+}
+
+type Stream struct {
+	token  string
+	ws     *websocket.Conn
+	Input  chan Message
+	Output chan Message
+}
+
+func LoopForever(token string, onMessage func(Message, chan Message)) error {
+	s, err := Connect(token)
+	if err != nil {
+		return err
+	}
+	defer s.Close()
+
+	for {
+		select {
+		case msg, ok := <-s.Input:
+			// Handle disconects.
+			if !ok {
+				s, err = Connect(token)
+				if err != nil {
+					return errors.New(fmt.Sprint("unable to reconnect:", err))
+				}
+				defer s.Close()
+				continue
+			}
+			go onMessage(msg, s.Output)
+		case <-time.After(timeout):
+			s.Output <- Message{"type": "ping"}
+		}
+	}
+}
+
+func Connect(token string) (s *Stream, err error) {
+	s = new(Stream)
+	s.token = token
+
+	var url string
+	url, err = s.webSocketUrl()
+	if err != nil {
+		return
+	}
+
+	s.ws, err = websocket.Dial(url, "", origin)
+	if err != nil {
+		return
+	}
+
+	s.Input = make(chan Message)
+	go func() {
+		for {
+			var msg Message
+			if err := websocket.JSON.Receive(s.ws, &msg); err != nil {
+				close(s.Input)
+				return
+			}
+			if msg != nil {
+				s.Input <- msg
+			}
+		}
+	}()
+
+	s.Output = make(chan Message)
+	go func() {
+		id := 0
+		for {
+			msg := <-s.Output
+			msg["id"] = id
+			if err := websocket.JSON.Send(s.ws, msg); err != nil {
+				close(s.Output)
+				return
+			}
+			id++
+		}
+	}()
+
+	return
+}
+
+func (s *Stream) webSocketUrl() (url string, err error) {
+	resp, err := http.Get("https://slack.com/api/rtm.start?token=" + s.token)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+	buf, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+	var body map[string]interface{}
+	if err = json.Unmarshal(buf, &body); err != nil {
+		return
+	}
+	url, ok := body["url"].(string)
+	if !ok {
+		err = errors.New("unable to parse rtm url")
+	}
+	return
+}
+
+func (s *Stream) Close() error {
+	return s.ws.Close()
+}
